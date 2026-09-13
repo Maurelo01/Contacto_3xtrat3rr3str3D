5da759b1a2ea167dbd304b15b91e990eed6adc8c
maurelo860@gmail.com <maurelo860@gmail.com>
2026-09-13 12:01:25 -0600

    Base Proyecto

---
+++ Contacto_3xtrat3rr3str3D/src/main/java/mycompany/contacto_3xtrat3rr3str3d/Contacto_3xtrat3rr3str3D.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mycompany.contacto_3xtrat3rr3str3d;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Contacto_3xtrat3rr3str3D {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
