fdc4c57241b188886b84209852aa2b7036fca6d2
maurelo860@gmail.com <maurelo860@gmail.com>
2026-09-25 14:23:10 -0600

    Se añadio la funcionalidad para llamar funciones y entornos

--- Contacto_3xtrat3rr3str3D/src/main/java/mycompany/contacto_3xtrat3rr3str3d/c3d/InstruccionesC3D.java
+++ Contacto_3xtrat3rr3str3D/src/main/java/mycompany/contacto_3xtrat3rr3str3d/c3d/InstruccionesC3D.java
@@ -5,74 +5,97 @@ import java.util.List;
 
 public class InstruccionesC3D
 {
-    private List<String> codigo;
+    private List<String> codigoMain;
+    private List<String> codigoFunciones;
+    private boolean escribiendoEnFuncion;
+
     public InstruccionesC3D()
     {
-        this.codigo = new ArrayList<>();
+        this.codigoMain = new ArrayList<>();
+        this.codigoFunciones = new ArrayList<>();
+        this.escribiendoEnFuncion = false;
     }
 
     public void limpiar()
     {
-        this.codigo.clear();
+        this.codigoMain.clear();
+        this.codigoFunciones.clear();
+        this.escribiendoEnFuncion = false;
+    }
+
+    public void setEscribiendoEnFuncion(boolean enFuncion)
+    {
+        this.escribiendoEnFuncion = enFuncion;
+    }
+
+    private void agregar(String instruccion)
+    {
+        if (escribiendoEnFuncion)
+        {
+            codigoFunciones.add(instruccion);
+        }
+        else
+        {
+            codigoMain.add(instruccion);
+        }
     }
 
     public void agregarEtiqueta(String etiqueta)
     {
-        codigo.add(etiqueta + ":");
+        agregar(etiqueta + ":");
     }
-
     public void agregarSaltoIncondicional(String etiqueta)
     {
-        codigo.add("goto " + etiqueta + ";");
+        agregar("goto " + etiqueta + ";");
     }
-
     public void agregarSaltoCondicional(String arg1, String op, String arg2, String etiqueta)
     {
-        codigo.add("if (" + arg1 + " " + op + " " + arg2 + ") goto " + etiqueta + ";");
+        agregar("if (" + arg1 + " " + op + " " + arg2 + ") goto " + etiqueta + ";");
     }
-
     public void agregarSetStack(String posicion, String valor)
     {
-        codigo.add("stack[(int)" + posicion + "] = " + valor + ";");
+        agregar("stack[(int)" + posicion + "] = " + valor + ";");
     }
-
     public void agregarGetStack(String destino, String posicion)
     {
-        codigo.add(destino + " = stack[(int)" + posicion + "];");
+        agregar(destino + " = stack[(int)" + posicion + "];");
     }
-
     public void agregarSetHeap(String posicion, String valor)
     {
-        codigo.add("heap[(int)" + posicion + "] = " + valor + ";");
+        agregar("heap[(int)" + posicion + "] = " + valor + ";");
     }
-
     public void agregarGetHeap(String destino, String posicion)
     {
-        codigo.add(destino + " = heap[(int)" + posicion + "];");
+        agregar(destino + " = heap[(int)" + posicion + "];");
     }
-
     public void agregarAsignacion(String destino, String arg1, String op, String arg2)
     {
-        codigo.add(destino + " = " + arg1 + " " + op + " " + arg2 + ";");
+        agregar(destino + " = " + arg1 + " " + op + " " + arg2 + ";");
     }
-
     public void agregarAsignacion(String destino, String valor)
     {
-        codigo.add(destino + " = " + valor + ";");
+        agregar(destino + " = " + valor + ";");
     }
-
     public void agregarComentario(String comentario)
     {
-        codigo.add("// " + comentario + "");
+        agregar("// " + comentario);
     }
-
     public void agregarPrint(String formato, String valor)
     {
-        codigo.add("printf(\"%" + formato + "\", " + valor + ");");
+        agregar("printf(\"%" + formato + "\", " + valor + ");");
+    }
+    
+    public void agregarCodigoBruto(String codigo)
+    {
+        agregar(codigo);
     }
 
-    public List<String> getCodigo()
+    public List<String> getCodigoMain()
     {
-        return codigo;
+        return codigoMain;
+    }
+    public List<String> getCodigoFunciones()
+    {
+        return codigoFunciones;
     }
 }
